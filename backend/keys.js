1ad773d63c72f03f98fd0cc8c371f1ead7708125
Vadodariya Jimesh M. <vadodariyajimesh1811@gmail.com>
2024-10-03 18:09:17 +0530

    first commit

---
+++ backend/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoUrl: "mongodb+srv://Jimesh:Gxpte99uzf7n56nb@cluster0.d8qx6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    Jwt_secret: "faslkfocvneofu"
+};
